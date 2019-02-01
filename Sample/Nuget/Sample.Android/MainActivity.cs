4bc5b2b3c28cd385ac7487f47861ced1d294bf81
Mason <mason.yunchen@gmail.com>
2019-02-01 14:28:08 +1300

    nuget sample update

--- Sample/Nuget/Sample.Android/MainActivity.cs
+++ Sample/Nuget/Sample.Android/MainActivity.cs
@@ -1,10 +1,5 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace Sample.Droid
@@ -18,8 +13,8 @@ namespace Sample.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);        
-            Xamarin.EnableKeyboardEffect.Droid.Effects.Init(this);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.EnableKeyboardEffect.Platform.Droid.Effects.Init(this);
             LoadApplication(new App());
         }
     }
--- Sample/Nuget/Sample/MainPage.xaml.cs
+++ Sample/Nuget/Sample/MainPage.xaml.cs
@@ -19,35 +19,5 @@ namespace Sample
         {
             Entry2.Focus();
         }
-
-        private void Entry1_OnCompleted(object sender, EventArgs e)
-        {
-            Entry3.Focus();
-        }
-
-        private void Entry3_OnCompleted(object sender, EventArgs e)
-        {
-            Entry4.Focus();
-        }
-
-        private void Entry4_OnCompleted(object sender, EventArgs e)
-        {
-            Entry5.Focus();
-        }
-
-        private void Entry5_OnCompleted(object sender, EventArgs e)
-        {
-            Entry6.Focus();
-        }
-
-        private void Entry6_OnCompleted(object sender, EventArgs e)
-        {
-            Entry2.Focus();
-        }
-
-        private void Entry2_OnCompleted(object sender, EventArgs e)
-        {
-            Entry1.Focus();
-        }
     }
 }
