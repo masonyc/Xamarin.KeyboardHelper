f98047e27771ceac3cd1df0137b5ac8249d4dce6
Mason(Yun) Chen <mason.yunchen@gmail.com>
2019-04-15 13:40:35 +1200

    remove lib script

--- nuget/Copylibs.ps1
+++
@@ -1,3 +0,0 @@
-Copy-Item "..\scr\Xamarin.EnableKeyboardEffect\bin\Release\Xamarin.EnableKeyboardEffect.*.nupkg" -Destination '.\Temp\nupkg.zip'
-Expand-Archive -Path '.\Temp\nupkg.zip' -DestinationPath Temp
-Copy-Item '.\Temp\lib' -Destination '.\' -Recurse -ErrorAction SilentlyContinue
