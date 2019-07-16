f611d298cf34cb94b9468e7e905a76874cc20433
Mason(Yun) Chen <mason.yunchen@gmail.com>
2019-07-17 11:17:40 +1200

    nuget sample update

--- Sample/Nuget/Sample/MainPage.xaml.cs
+++ Sample/Nuget/Sample/MainPage.xaml.cs
@@ -13,20 +13,6 @@ namespace Sample
             this.Disappearing += MainPage_Disappearing;
         }
 
-        private void MainPage_Disappearing(object sender, EventArgs e)
-        {
-            SoftKeyboard.Current.VisibilityChanged -= Current_VisibilityChanged;
-        }
-
-        private void MainPage_Appearing(object sender, EventArgs e)
-        {
-            SoftKeyboard.Current.VisibilityChanged += Current_VisibilityChanged;
-        }
-
-        private void Current_VisibilityChanged(SoftKeyboardEventArgs e)
-        {
-            LabelMessage.Text = $"KeyBoard is visible : {(e.IsVisible ? "Yes" : "No")}";
-        }
         private void button1_clicked(object sender, EventArgs e)
         {
             Entry1.Focus();
@@ -35,6 +21,21 @@ namespace Sample
         private void button2_clicked(object sender, EventArgs e)
         {
             Entry2.Focus();
+        }
+
+        private void Current_VisibilityChanged(SoftKeyboardEventArgs e)
+        {
+            LabelMessage.Text = $"KeyBoard is visible : {(e.IsVisible ? "Yes" : "No")}";
+        }
+
+        private void MainPage_Appearing(object sender, EventArgs e)
+        {
+            SoftKeyboard.Current.VisibilityChanged += Current_VisibilityChanged;
+        }
+
+        private void MainPage_Disappearing(object sender, EventArgs e)
+        {
+            SoftKeyboard.Current.VisibilityChanged -= Current_VisibilityChanged;
         }
     }
 }
